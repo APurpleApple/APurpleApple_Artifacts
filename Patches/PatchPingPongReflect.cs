9efefada0e639e8bd0cdefdfc51c4a80e3777e38
Michał Dolaś <me@shockah.pl>
2025-03-04 11:13:21 +0100

    Added an artifact pulse to Ping Pong Armor

--- Patches/PatchPingPongReflect.cs
+++ Patches/PatchPingPongReflect.cs
@@ -15,7 +15,7 @@ namespace APurpleApple.GenericArtifacts.Patches
         [HarmonyPatch(typeof(AAttack), nameof(AAttack.Begin)), HarmonyPrefix]
         public static void AfterAttackTryBouncePatch(AAttack __instance, State s, Combat c, G g)
         {
-            if (!s.artifacts.Any(a => a is ArtifactPingPong)) { return; }
+            if (s.artifacts.OfType<ArtifactPingPong>().FirstOrDefault() is not { } artifact) { return; }
 
             Ship ship = (__instance.targetPlayer ? s.ship : c.otherShip);
             Ship ship2 = (__instance.targetPlayer ? c.otherShip : s.ship);
@@ -50,6 +50,7 @@ namespace APurpleApple.GenericArtifacts.Patches
                 weaken = __instance.weaken,
                 moveEnemy = __instance.moveEnemy,
                 paybackCounter = __instance.paybackCounter,
+                artifactPulse = artifact.Key(),
             };
             c.QueueImmediate(attack);
         }
